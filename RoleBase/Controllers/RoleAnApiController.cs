d80bbbd4108b2c2cedac9f131fcdb0b8f0294579
kevan <kingex1124@gmail.com>
2020-04-22 14:57:01 +0800

    1.角色編輯畫面，新增、編輯、刪除功能API撰寫 2.API路由設定調整

--- RoleBase/Controllers/RoleAnApiController.cs
+++ RoleBase/Controllers/RoleAnApiController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -14,12 +15,66 @@ namespace RoleBase.Controllers
     {
         RoleService roleService = new RoleService();
 
+        /// <summary>
+        /// 取得角色資料
+        /// </summary>
+        /// <returns></returns>
         [EnableCors(origins: "*", headers: "*", methods: "GET, POST, PUT, DELETE, OPTIONS")]
         [HttpPost]
         public IEnumerable<RoleVO> RoleAddEditDelete()
         {
+            // Thread.Sleep(1000);
             var roleData = roleService.GetRoleData();
             return roleData;
+        }
+
+        /// <summary>
+        /// 新增Role
+        /// </summary>
+        /// <param name="roleVO"></param>
+        /// <returns></returns>
+        [EnableCors(origins: "*", headers: "*", methods: "GET, POST, PUT, DELETE, OPTIONS")]
+        [HttpPost]
+        public RoleVO AddRole(RoleVO roleVO)
+        {
+            if (!ModelState.IsValid)
+                roleVO.Message = "請填寫必填欄位";
+            else
+            {
+                var result = roleService.AddRole(roleVO);
+
+                if (!string.IsNullOrEmpty(result))
+                    roleVO.Message = result;
+            }
+            return roleVO;
+        }
+
+        /// <summary>
+        /// 刪除Role
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [EnableCors(origins: "*", headers: "*", methods: "GET, POST, PUT, DELETE, OPTIONS")]
+        [HttpPost]
+        public string DeleteRole(RoleVO roleVO)
+        {
+            var result = roleService.DeleteRole(roleVO.RoleID.ToString());
+
+            return result;
+        }
+
+        /// <summary>
+        /// 編輯Role
+        /// </summary>
+        /// <param name="roleVO"></param>
+        /// <returns></returns>
+        [EnableCors(origins: "*", headers: "*", methods: "GET, POST, PUT, DELETE, OPTIONS")]
+        [HttpPost]
+        public string EditRole(RoleVO roleVO)
+        {
+            var result = roleService.EditRole(roleVO);
+           
+            return result;
         }
     }
 }
