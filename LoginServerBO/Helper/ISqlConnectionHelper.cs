7ad3489a0239a9da28cda727c6969064953fb63e
kevan.chen <kevan.chen@sinopa.com>
2020-07-20 17:34:14 +0800

    1.完成RoleService單元測試 2.完成RoleBO單元測試 3.從RoleBO拆出SQLConnectionHelp

---
+++ LoginServerBO/Helper/ISqlConnectionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginServerBO.Helper
+{
+    public interface ISqlConnectionHelper
+    {
+        SqlConnection GetSQLConnection();
+        SqlTransaction GetSqlTransaction(SqlConnection conn);
+        void CommitTrans(SqlTransaction trans);
+        void Rollback(SqlTransaction trans);
+    }
+}
