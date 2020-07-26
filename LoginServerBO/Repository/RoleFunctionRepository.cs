8e1179bb0ee1dd68b9f97c836e266e445de89cc0
kevan <kingex1124@gmail.com>
2020-07-26 23:29:35 +0800

    1.更新kevanFramework 2.加入窮人DI

--- LoginServerBO/Repository/RoleFunctionRepository.cs
+++ LoginServerBO/Repository/RoleFunctionRepository.cs
@@ -24,8 +24,8 @@ namespace LoginServerBO.Repository
 
         public RoleFunctionRepository()
         {
-            DataAccessIO.Register<IDataAccess, DataAccess>();
-            _dataAccess = (DataAccess)DataAccessIO.Resolve<IDataAccess>("AccountConn");
+            UnityContainer.Register<IDataAccess, DataAccess>();
+            _dataAccess = UnityContainer.Resolve<IDataAccess>("AccountConn");
         }
 
         public RoleFunctionRepository(IDataAccess dataAccess)
