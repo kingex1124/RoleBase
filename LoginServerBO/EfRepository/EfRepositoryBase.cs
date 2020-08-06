aa5e79b6034e851c1e8e38f454d4f6d8b3b57506
kevan.chen <kevan.chen@sinopa.com>
2020-08-06 11:05:06 +0800

    新增註解 部分程式碼重構

--- LoginServerBO/EfRepository/EfRepositoryBase.cs
+++ LoginServerBO/EfRepository/EfRepositoryBase.cs
@@ -13,49 +13,153 @@ namespace LoginServerBO.EfRepository
 {
     public class EfRepositoryBase<TEntity, TDbContext> : IRepository<TEntity> where TEntity : class where TDbContext : DbContext
     {
+        #region 屬性
+
         protected TDbContext _db;
-        protected DbSet<TEntity> Set;
+        protected DbSet<TEntity> _set;
         private int _updateCount = 0;
+
+        #endregion
+
+        #region 建構子
 
         public EfRepositoryBase(TDbContext db)
         {
             _db = db;
-            Set = db.Set<TEntity>();
+            _set = db.Set<TEntity>();
         }
 
+        #endregion
+
+        #region 方法
+
+        #region 新增寫入
+
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="entity"></param>
         public void Insert(TEntity entity)
         {
-            Set.Add(entity);
+            _set.Add(entity);
             _updateCount++;
         }
 
+        /// <summary>
+        /// 多筆資料寫入
+        /// </summary>
+        /// <param name="entities"></param>
+        public void InsertRange(IEnumerable<TEntity> entities)
+        {
+            _set.AddRange(entities);
+            _updateCount += entities.Count();
+        }
+
+        /// <summary>
+        /// 大量寫入的方法
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <param name="createUser"></param>
+        /// <param name="sqlBulkCopyOptions"></param>
+        /// <param name="batchSize"></param>
+        public void BulkInsert(IEnumerable<TEntity> entities, string createUser, SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default, int? batchSize = null)
+        {
+            foreach (var entity in entities)
+            {
+                typeof(TEntity).GetProperty("Gid", typeof(Guid))?.SetValue(entity, Guid.NewGuid(), null);
+                typeof(TEntity).GetProperty("CreatedUser", typeof(string))?.SetValue(entity, createUser, null);
+                typeof(TEntity).GetProperty("CreatedDate", typeof(DateTime))?.SetValue(entity, DateTime.Now, null);
+                typeof(TEntity).GetProperty("UpdatedUser", typeof(string))?.SetValue(entity, createUser, null);
+                typeof(TEntity).GetProperty("UpdatedDate", typeof(DateTime))?.SetValue(entity, DateTime.Now, null);
+            }
+            _db.BulkInsert(entities, sqlBulkCopyOptions, batchSize);
+        }
+
+        #endregion
+
+        #region 修改
+
+        /// <summary>
+        /// 單筆資料異動
+        /// </summary>
+        /// <param name="entity"></param>
         public void Update(TEntity entity)
         {
             _db.Entry(entity).State = EntityState.Modified;
             _updateCount++;
         }
+
+        #endregion
+
+        #region 刪除
+
+        /// <summary>
+        /// 單筆資料刪除
+        /// </summary>
+        /// <param name="entity"></param>
         public void Delete(TEntity entity)
         {
-            Set.Remove(entity);
+            _set.Remove(entity);
             _updateCount++;
         }
 
-        public void InsertRange(IEnumerable<TEntity> entities)
+        /// <summary>
+        /// 透過key刪除資料
+        /// </summary>
+        /// <param name="keys"></param>
+        public void DeleteItemsByKeys(object[] keys)
         {
-            Set.AddRange(entities);
-            _updateCount += entities.Count();
+            foreach (var key in keys)
+                DeleteByKey(key);
         }
 
+        /// <summary>
+        /// 透過key刪除資料
+        /// </summary>
+        /// <param name="keys"></param>
+        public void DeleteByKey(params object[] keys)
+        {
+            var item = _set.Find(keys);
+            if (item == null) return;
+            if (typeof(IDeleteState).IsAssignableFrom(typeof(TEntity)))
+            {
+                var alternate = (IDeleteState)item;
+                alternate.IsDeleted = true;
+                Update(item);
+            }
+            else
+                Delete(item);
+        }
+
+
+        #endregion
+
+        #region 查詢
+
+        /// <summary>
+        /// 查找條件資料
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         public IQueryable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate)
         {
-            return Set.Where(predicate);
+            return _set.Where(predicate);
         }
 
-        public IQueryable<TEntity> GetAll()
+        /// <summary>
+        /// 取得單筆資料
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
         {
-            return Set;
+            return _set.FirstOrDefault(predicate);
         }
 
+        /// <summary>
+        /// 取得List資料
+        /// </summary>
+        /// <returns></returns>
         public IQueryable<TEntity> GetAvailable()
         {
             var queryable = GetAll();
@@ -70,21 +174,41 @@ namespace LoginServerBO.EfRepository
             return queryable;
         }
 
-        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        /// <summary>
+        /// 取得所有資料
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<TEntity> GetAll()
         {
-            return Set.FirstOrDefault(predicate);
+            return _set;
         }
 
+        /// <summary>
+        /// 透過key查找資料
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
         public TEntity Find(params object[] keys)
         {
-            return Set.Find(keys);
+            return _set.Find(keys);
         }
 
+        /// <summary>
+        /// 查找資料
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         public TEntity Find(Expression<Func<TEntity, bool>> predicate)
         {
-            return Set.SingleOrDefault(predicate);
+            return _set.SingleOrDefault(predicate);
         }
 
+        #endregion
+
+        /// <summary>
+        /// 儲存異動
+        /// </summary>
+        /// <returns></returns>
         public int SaveChanges()
         {
             try
@@ -98,52 +222,36 @@ namespace LoginServerBO.EfRepository
             }
         }
 
-        public void DeleteByKey(params object[] keys)
-        {
-            var item = Set.Find(keys);
-            if (item == null) return;
-            if (typeof(IDeleteState).IsAssignableFrom(typeof(TEntity)))
-            {
-                var alternate = (IDeleteState)item;
-                alternate.IsDeleted = true;
-                Update(item);
-            }
-            else
-                Delete(item);
-        }
-
-        public void DeleteItemsByKeys(object[] keys)
-        {
-            foreach (var key in keys)
-                DeleteByKey(key);
-        }
-
-        public void BulkInsert(IEnumerable<TEntity> entities, string createUser, SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default, int? batchSize = null)
-        {
-            foreach (var entity in entities)
-            {
-                typeof(TEntity).GetProperty("Gid", typeof(Guid))?.SetValue(entity, Guid.NewGuid(), null);
-                typeof(TEntity).GetProperty("CreatedUser", typeof(string))?.SetValue(entity, createUser, null);
-                typeof(TEntity).GetProperty("CreatedDate", typeof(DateTime))?.SetValue(entity, DateTime.Now, null);
-                typeof(TEntity).GetProperty("UpdatedUser", typeof(string))?.SetValue(entity, createUser, null);
-                typeof(TEntity).GetProperty("UpdatedDate", typeof(DateTime))?.SetValue(entity, DateTime.Now, null);
-            }
-            _db.BulkInsert(entities, sqlBulkCopyOptions, batchSize);
-        }
-
+        /// <summary>
+        /// 透過SQLString做新刪修
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
         public void ExecuteSqlCommand(string sql, params object[] parameters)
         {
             _db.Database.ExecuteSqlCommand(sql, parameters);
         }
 
+        /// <summary>
+        /// 設置DbContext
+        /// </summary>
+        /// <param name="db"></param>
         public void SetDbContext(TDbContext db)
         {
             _db = db;
         }
 
+        /// <summary>
+        /// EF的異動偵測DetectChanges機制
+        /// DetectChanges為true時，每次異動都會做差異比對
+        /// 所以大量處理資料時會先設為false,最後再改為true做一次差異比對
+        /// </summary>
+        /// <param name="value"></param>
         public void SetDbAutoDetectChangesEnabled(bool value)
         {
             _db.Configuration.AutoDetectChangesEnabled = value;
         }
+
+        #endregion
     }
 }
