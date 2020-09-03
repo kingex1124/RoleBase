55dc6d72c026ba73bff6dcb1bb581ba8e2ee1a9c
kevan.chen <kevan.chen@sinopa.com>
2020-09-03 16:58:29 +0800

    刪除多於程式碼

--- Login.DAL/Interceptor/MSSQLInterceptorLogging.cs
+++ Login.DAL/Interceptor/MSSQLInterceptorLogging.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.Entity.Infrastructure.Interception;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +9,14 @@ using System.Threading.Tasks;
 namespace Login.DAL
 {
     public class MSSQLInterceptorLogging : DbCommandInterceptor
-    {
-        private readonly Stopwatch _stopwatch = new Stopwatch();
-
+    {   
         public override void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
         {
             base.ScalarExecuting(command, interceptionContext);
-            _stopwatch.Restart();
         }
 
         public override void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
         {
-            _stopwatch.Stop();
             if (interceptionContext.Exception != null)
             {
               
@@ -36,12 +31,10 @@ namespace Login.DAL
         public override void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
         {
             base.NonQueryExecuting(command, interceptionContext);
-            _stopwatch.Restart();
         }
 
         public override void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
         {
-            _stopwatch.Stop();
             if (interceptionContext.Exception != null)
             {
                
@@ -56,11 +49,9 @@ namespace Login.DAL
         public override void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
         {
             base.ReaderExecuting(command, interceptionContext);
-            _stopwatch.Restart();
         }
         public override void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
         {
-            _stopwatch.Stop();
             if (interceptionContext.Exception != null)
             {
               
