6d019ffc26269e4764c883cae4ce73e6a2b4c1b3
kevan.chen <kevan.chen@sinopa.com>
2020-10-07 15:08:27 +0800

    ExcuteResult Change

--- Login.VO/VO/ExecuteResult.cs
+++ Login.VO/VO/ExecuteResult.cs
@@ -22,5 +22,39 @@ namespace Login.VO
             IsSuccessed = isSuccessed;
             Message = message;
         }
+
+        public static ExecuteResult Ok()
+        {
+            return new ExecuteResult { IsSuccessed = true };
+        }
+
+        public static ExecuteResult Fail(string errMsg)
+        {
+            return new ExecuteResult { IsSuccessed = false, Message = errMsg };
+        }
+    }
+
+    public class ExecuteResult<T> : ExecuteResult
+    {
+        public T data { get; set; }
+
+        public ExecuteResult()
+        {
+            data = default(T);
+        }
+        public static ExecuteResult<T> Ok(T data)
+        {
+            return new ExecuteResult<T> { IsSuccessed = true, data = data };
+        }
+
+        public static ExecuteResult<T> Ok(T data, string msg)
+        {
+            return new ExecuteResult<T> { IsSuccessed = true, Message = msg, data = data };
+        }
+
+        public static ExecuteResult<T> Fail(string errMsg)
+        {
+            return new ExecuteResult<T> { IsSuccessed = false, Message = errMsg };
+        }
     }
 }
